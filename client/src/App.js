cde5cb0c8708a9d64d690b353d64f52664b8b822
william arasirwa <mageboarasirwa03@gmail.com>
2023-07-01 23:51:21 +0300

    third commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import Home from './components/Home';
-import About from './components/About';
-import Booking from './components/Booking';
-import Reviews from './components/Reviews';
-
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Booking from "./pages/Booking";
+import Reviews from "./pages/Reviews";
+import "./App.css";
+import Layout from "./layout/Layout";
+import React from "react";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <Routes>
-        <Route  path = "/" element = { <Home/> } />
-        <Route path = "About" element = {<About/>} />
-        <Route path = "Booking" element = {<Booking/>} />
-        <Route path = "Reviews" element = {<Reviews/>} />
-        <Route path = "Logout" element = {<Logout/>} />
-        
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="booking" element={<Booking />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-
   );
 }
 
