88521605101797fb8ac66d525dc36d25dbb80136
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-07-01 22:12:13 +0300

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import Booking from './components/Booking';
+import Reviews from './components/Reviews';
 
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route  path = "/" element = { <Home/> } />
+        <Route path = "About" element = {<About/>} />
+        <Route path = "Booking" element = {<Booking/>} />
+        <Route path = "Reviews" element = {<Reviews/>} />
+        <Route path = "Logout" element = {<Logout/>} />
+        
+      </Routes>
+    </BrowserRouter>
     </div>
 
   );
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">FlyGo</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <a className="nav-link active" aria-current="page" href="#">Home</a>
+        <a className="nav-link" href="#">About</a>
+        <a className="nav-link" href="#">Booking</a>
+        <a className="nav-link disabled">Reviews</a>
+        <a className="nav-link disabled">Logout</a>
+      </div>
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default Navbar
+
