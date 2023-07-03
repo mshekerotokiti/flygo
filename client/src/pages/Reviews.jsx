6ea81ef5a76a01bd25320db29f9d2d86a571cb52
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-07-03 10:48:41 +0300

    reviews component

--- client/src/pages/Reviews.jsx
+++ client/src/pages/Reviews.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 const Reviews = () => {
-  return <div>Reviews</div>;
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    fetchReviews();
+  }, []);
+
+  const fetchReviews = async () => {
+    try {
+      const response = await fetch('/reviews');
+      const data = await response.json();
+      setReviews(data);
+    } catch (error) {
+      console.error('Error fetching reviews:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>What FlyGo users are saying about the services:</h2>
+      <ul>
+        {reviews.map((review) => (
+          <li key={review.id}>
+            <h3>{review.name}</h3>
+            <p>{review.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Reviews;
+
