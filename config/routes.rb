135e77b1431a2307e93ddf11e9208e767a1351f9
+ <+>
2023-07-03 09:58:17 +0300

    Created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :airlines
+  resources :reviews, only: [:show, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
